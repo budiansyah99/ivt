b2584118a82832cd65feab4326d16520334017b4
budiansyah99 <61037741+budiansyah99@users.noreply.github.com>
2020-07-15 21:22:58 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,32 @@
 
+<?php
+$host = 'server-mysql.mysql.database.azure.com';
+$username = 'budi@server-mysql';
+$password = 'Jakarta123';
+$db_name = 'quickstartdb';
+
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+//Run the Select query
+printf("Reading data from table: \n");
+$res = mysqli_query($conn, 'SELECT * FROM inventory');
+while ($row = mysqli_fetch_assoc($res)) {
+var_dump($row);
+}
+
+//<br><br>
+  //printf("
+  //Link AJa
+  //<br>
+ // 
+echo "<table align=center><tr><th><a href=https://lab-budi.azurewebsites.net/config.php>No</a></th><th>Budiansyah</th><th>Alamat</th></tr></table>";
+//Close the connection
+ // ?>
+
+
+?>
