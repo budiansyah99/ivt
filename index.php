f4ad1da46ae89d488c3cc0fefe47f2ebda357c3d
budiansyah99 <61037741+budiansyah99@users.noreply.github.com>
2020-07-15 20:54:42 +0700

    Update index.php
    ubah

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $host = 'server-mysql.mysql.database.azure.com';
 $username = 'budi@server-mysql';
 $password = 'Jakarta123';
-$db_name = 'budiweb';
+$db_name = 'quickstartdb';
+
 
 //Establishes the connection
 $conn = mysqli_init();
@@ -10,20 +11,12 @@ mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-
-// Run the create table query
-if (mysqli_query($conn, '
-CREATE TABLE Products (
-`Id` INT NOT NULL AUTO_INCREMENT ,
-`ProductName` VARCHAR(200) NOT NULL ,
-`Color` VARCHAR(50) NOT NULL ,
-`Price` DOUBLE NOT NULL ,
-PRIMARY KEY (`Id`)
-);
-')) {
-printf("Table created\n");
+//Run the Select query
+printf("Reading data from table: \n");
+$res = mysqli_query($conn, 'SELECT * FROM inventory');
+while ($row = mysqli_fetch_assoc($res)) {
+var_dump($row);
 }
-
 //Close the connection
 mysqli_close($conn);
 ?>
